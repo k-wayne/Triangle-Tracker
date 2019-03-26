9a4f22f1849991ea27927537ff9b586f29166c04
kwayne <wkudoi@gmail.com>
2019-03-26 12:31:48 +0300

    add not a number code improve

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@ function trackMaster () {
 var a= parseInt(document.getElementById('a').value);
 var b= parseInt(document.getElementById('b').value);
 var c= parseInt(document.getElementById('c').value);
+
 var triangle=["Equilateral", "Isosceles", "Scalene", "Wrong value(s). Please retry!"];
 if(a===b && b===c && a===c){
    alert(triangle[0]);
  }
- else if(a===b ||a===c|| b===c){
+ else if(a===b && a + b > c ||a===c && a + c > b || b===c && b + c > a){
    alert(triangle[1]);
  }
  else if ( a + b < c || b + c < a || a + c < b){
